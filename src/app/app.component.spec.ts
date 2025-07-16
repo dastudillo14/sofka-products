76ac4ee25e3d894589a314938688872c645c4311
Dantee Astudillo <dantee-mauri@hotmail.com>
2025-07-15 21:32:38 -0500

    chg names

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ntt-data-test'`, () => {
+  it(`should have as title 'test-product'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ntt-data-test');
+    expect(app.title).toEqual('test-product');
   });
  
 });
